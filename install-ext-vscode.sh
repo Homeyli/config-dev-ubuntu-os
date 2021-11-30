b2a70a1bf8840b9c42df81b460ec779233948f64
Mahdi Homeyli <homeily@gmail.com>
2021-11-30 11:37:51 +0330

    add ext-vscode file

---
+++ install-ext-vscode.sh
@@ -0,0 +1,36 @@
+#GoLang 
+code --install-extension golang.Go ;
+
+# PHP 
+code --install-extension kokororin.vscode-phpfmt;
+
+# Laravel
+code --install-extension onecentlin.laravel-blade && \
+code --install-extension onecentlin.laravel5-snippets && \
+code --install-extension austenc.laravel-blade-spacer && \
+code --install-extension ryannaddy.laravel-artisan && \
+code --install-extension amiralizadeh9480.laravel-extra-intellisense && \
+code --install-extension stef-k.laravel-goto-controller && \
+code --install-extension codingyu.laravel-goto-view && \
+
+# WEB
+code --install-extension octref.vetur && \
+code --install-extension ecmel.vscode-html-css;
+
+# ICON PACK
+code --install-extension PKief.material-icon-theme;
+
+# DOCKER 
+code --install-extension ms-azuretools.vscode-docker;
+
+# GIT
+code --install-extension eamodio.gitlens;
+
+# OTHER
+code --install-extension mikestead.dotenv;
+
+
+
+
+
+
